ede4a917f9cb1cdaebd0cdea3f9b1ec04b01c359
amanritesh94 <amanritesh94@gmail.com>
2024-03-15 12:41:11 +0530

    Implement singly link list

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -1,25 +1,32 @@
 class Node {
   constructor(val) {
+    // Node value
     this.val = val
+    // Pointer to the next node in the list
     this.next = null
   }
 }
 
 class SinglyLinkList {
   constructor() {
+    // Head of the singly linked list
     this.head = null
+    // Tail of the singly linked list
     this.tail = null
+    // Length of the singly linked list
     this.length = 0
   }
 
-  //Add item at the end of singly link list
+  // Add item at the end of the singly linked list
   push(val) {
     const node = new Node(val)
     if (this.head === null) {
+      // If list is empty, set head and tail to the new node
       this.head = node
       this.tail = node
       this.length++
     } else {
+      // Otherwise, add new node to the tail
       this.tail.next = node
       this.tail = node
       this.length++
@@ -28,13 +35,15 @@ class SinglyLinkList {
     return this
   }
 
-  // Remove the item from end of singly list
+  // Remove the item from the end of the singly linked list
   pop() {
     if (this.head === null) {
+      // If list is empty, return null
       return null
     } else {
       let temp = this.head
       while (temp.next.next) {
+        // Traverse the list to find the second-to-last node
         temp = temp.next
       }
       this.tail = temp
@@ -43,6 +52,7 @@ class SinglyLinkList {
       this.length--
 
       if (this.length === 0) {
+        // If list becomes empty, update head and tail
         this.head = null
         this.tail = null
       }
@@ -51,16 +61,19 @@ class SinglyLinkList {
     }
   }
 
-  // Remove the item from the begninig of link list
+  // Remove the item from the beginning of the linked list
   shift() {
     if (this.head === null) {
+      // If list is empty, return null
       return null
     } else {
       let temp = this.head
+      // Update head to the next node
       this.head = temp.next
       this.length--
 
       if (this.length === 0) {
+        // If list becomes empty, update head and tail
         this.head = null
         this.tail = null
       }
@@ -68,15 +81,17 @@ class SinglyLinkList {
     }
   }
 
-  // Add the item at the begninig of link list
+  // Add the item at the beginning of the linked list
   unshift(val) {
     const node = new Node(val)
     if (this.head == null) {
+      // If list is empty, set head and tail to the new node
       this.head = node
       this.tail = node
       this.length++
       return this
     } else {
+      // Otherwise, add new node to the beginning
       node.next = this.head
       this.head = node
       this.length++
@@ -85,14 +100,17 @@ class SinglyLinkList {
     }
   }
 
-  // Insert item at particulat index in link list
+  // Insert item at a particular index in the linked list
   insert(index, val) {
     if (index > this.length) {
+      // If index is out of range, return null
       return null
     } else if (index == 0) {
+      // If index is 0, call unshift method to add at the beginning
       this.unshift(val)
       return this
     } else if (index == this.length) {
+      // If index is at the end, call push method to add at the end
       this.push(val)
       return this
     } else {
@@ -100,10 +118,12 @@ class SinglyLinkList {
       let temp = this.head
       let count = 1
       while (count < index) {
+        // Traverse the list to find the node at the given index
         temp = temp.next
         count++
       }
 
+      // Insert the new node at the specified index
       node.next = temp.next
       temp.next = node
       this.length++
@@ -112,31 +132,36 @@ class SinglyLinkList {
     }
   }
 
-  // Remove item at particular position in link list
+  // Remove item at a particular position in the linked list
   remove(index) {
     if (index > this.length) {
+      // If index is out of range, return null
       return null
     } else if (index === 0) {
+      // If index is 0, call shift method to remove from the beginning
       this.shift()
       return this
     } else if (index == this.length) {
+      // If index is at the end, call pop method to remove from the end
       this.pop()
       return this
     } else {
       let temp = this.head
       let count = 1
       while (count < index - 1) {
+        // Traverse the list to find the node before the index
         temp = temp.next
         count++
       }
 
+      // Remove the node at the specified index
       temp.next = temp.next.next
-      this.length++
+      this.length--
       return this
     }
   }
 
-  // Reverse link list
+  // Reverse the linked list
   reverse() {
     let current = null
     let newCurrent = null
@@ -152,4 +177,4 @@ class SinglyLinkList {
   }
 }
 
-const singly = new SinglyList()
+const singly = new SinglyLinkList()
